6352d6a9bac85d2025b48bd870397d444f9d44e7
joseflorezv <jose.florez@bemyself.com>
2025-08-13 15:27:27 -0500

    Update updateVersion.js

--- updateVersion.js
+++ updateVersion.js
@@ -21,7 +21,7 @@ const diffDays = Math.floor((now - lastSprintDate) / (1000 * 60 * 60 * 24));
 
 let [major, minor, patch] = packageJson.version.split('.').map(Number);
 
-if (diffDays >= 15) {
+if (diffDays >= 0) {
   // Nuevo sprint: reinicia patch y aumenta minor
   //prueba
 
